f551161aa4765ca7b9d1946f25184fe72ce97241
SBC <iamsbc@gmail.com>
2018-09-12 22:23:25 +0800

    add zxing lib & add CSVParser and QRCoder classes

--- QRCodeGenerator/CSVParser.cs
+++ QRCodeGenerator/CSVParser.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace QRCodeGenerator
 {
-    internal class Program
+    public class CSVParser
     {
-        private static void Main(string[] args)
-        {
-
-        }
     }
 }
--- QRCodeGenerator/QRCoder.cs
+++ QRCodeGenerator/QRCoder.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace QRCodeGenerator
 {
-    internal class Program
+    public class QRCoder
     {
-        private static void Main(string[] args)
-        {
-
-        }
     }
 }
