7cdb4dbf6c0c4fe33ad1dc79390e73e19f63513d
SBC <iamsbc@gmail.com>
2018-12-08 23:38:12 +0800

    QRCoder done

--- QRCodeGenerator/QRCoder.cs
+++ QRCodeGenerator/QRCoder.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
+using ZXing;
+using ZXing.QrCode;
 
 namespace QRCodeGenerator
 {
     public class QRCoder
     {
+
+        public static Bitmap Encode(string content)
+        {
+            var writer = new BarcodeWriter()
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new QrCodeEncodingOptions() { Height = 200, Width = 200 }
+            };
+
+            return writer.Write(content);
+        }
+
     }
 }
